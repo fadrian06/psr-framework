d2b5f4ea17545ed72f2a941f25539fba103016db
fadrian06 <franyeradriansanchez@gmail.com>
2026-09-17 00:29:23 -0400

    feat(functions): add type-safe getenv helper function
    Add a getenv function to retrieve and cast environment variables from
    $_ENV. It automatically converts values to boolean, integer, or float
    types using filter_var. This provides a more reliable way to handle
    configuration values that are not strictly strings.

--- src/functions.php
+++ src/functions.php
@@ -6,6 +6,29 @@ namespace Faslatam\PsrFramework;
 
 use Psr\Http\Message\ResponseInterface;
 
+function getenv(string $name): null|int|float|string|bool
+{
+  $env = $_ENV[$name] ?? null;
+
+  if ($filteredVar = filter_var($env, FILTER_VALIDATE_BOOL)) {
+    return $filteredVar;
+  }
+
+  if ($filteredVar = filter_var($env, FILTER_VALIDATE_INT)) {
+    return $filteredVar;
+  }
+
+  if ($filteredVar = filter_var($env, FILTER_VALIDATE_FLOAT)) {
+    return $filteredVar;
+  }
+
+  if (is_string($env)) {
+    return $env;
+  }
+
+  return null;
+}
+
 function sendResponse(ResponseInterface $response): void
 {
   http_response_code($response->getStatusCode());
